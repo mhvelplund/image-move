8fb27fb0e9cf32ddb284cb03936d45f8f2ad5fc9
Mads Hvelplund <mhvelplund@users.noreply.github.com>
2025-02-12 11:32:57 +0100

    chore: refactor for testability

--- imgmv/imgmv.py
+++ imgmv/imgmv.py
@@ -1,27 +1,51 @@
-import sys
 from pathlib import Path
 from shutil import copy, move
+from typing import Tuple
 
 
 def move_images(
-    source_path: Path,
-    destination_path: Path,
-    copy_file: bool,
-    prefix: str,
-    verbose: bool,
+  source_path: Path,
+  destination_path: Path,
+  copy_file: bool,
+  prefix: str,
+  verbose: bool = False,
+  dry_run: bool = False,
 ):
-    source_files = [f for f in source_path.iterdir() if f.is_file()]
+  source_files = [f for f in source_path.iterdir() if f.is_file()]
 
-    index = 0
-    for source_file in source_files:
-        cmd = copy if copy_file else move
-        new_name = f"{prefix}_{index}{source_file.suffix}"
-        index += 1
-        destination_file = Path(destination_path, new_name)
-        if verbose:
-            print(
-                f"{'copy' if copy_file else 'move'} {source_file} -> {destination_file}",
-                file=sys.stderr,
-            )
+  def nop(*args, **kwargs):
+    pass
 
-        cmd(source_file, destination_file)
+  cmd = nop if dry_run else copy if copy_file else move
+  cmd_name = "copy" if copy_file else "move"
+
+  for source_file, destination_file in generate_source_destination_pairs(source_files, destination_path, prefix):
+    if verbose:
+      print(f"{cmd_name} {source_file} -> {destination_file}")
+    cmd(source_file, destination_file)
+
+
+def generate_source_destination_pairs(
+  source_files: list[Path], destination_path: Path, prefix: str
+) -> list[Tuple[Path, Path]]:
+  """
+  Generate a list of source and destination file path pairs.
+  This function takes a list of source file paths, a destination directory path,
+  and a prefix string. It generates destination file paths by appending the prefix
+  and an index to the original file name, preserving the original file extension.
+  Args:
+    source_files (list[Path]): A list of source file paths.
+    destination_path (Path): The destination directory path.
+    prefix (str): The prefix to be added to the destination file names.
+  Returns:
+    list[Tuple[Path, Path]]: A list of tuples, each containing a source file path
+    and the corresponding destination file path.
+  """
+
+  def generate_destination_file(destination_path: Path, prefix: str, index: int, source_file: Path) -> Path:
+    return Path(destination_path, f"{prefix}_{index}{source_file.suffix}")
+
+  return [
+    (source_file, generate_destination_file(destination_path, prefix, index, source_file))
+    for index, source_file in enumerate(source_files)
+  ]
--- imgmv/main.py
+++ imgmv/main.py
@@ -6,76 +6,68 @@ from .imgmv import move_images
 
 
 def run():
-    parser = argparse.ArgumentParser(
-        prog="imgmv",
-        description="Rename images based on their folder.",
-    )
-    parser.add_argument(
-        "source",
-        type=str,
-        help="the source folder containing the images",
-    )
-    parser.add_argument(
-        "destination",
-        type=str,
-        help='the target folder to put the renamed images. Defaults to "."',
-        nargs="?",
-        default=".",
-    )
-    parser.add_argument(
-        "-c",
-        "--copy",
-        action="store_true",
-        help="copy instead of moving",
-    )
-    parser.add_argument(
-        "-V",
-        "--verbose",
-        action="store_true",
-        help="log file actions",
-    )
-    parser.add_argument(
-        "-p",
-        "--prefix",
-        type=str,
-        help="the image prefix to use. Defaults to the source folder name",
-    )
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser(
+    prog="imgmv",
+    description="Rename images based on their folder.",
+  )
+  parser.add_argument(
+    "source",
+    type=str,
+    help="the source folder containing the images",
+  )
+  parser.add_argument(
+    "destination",
+    type=str,
+    help='the target folder to put the renamed images. Defaults to "."',
+    nargs="?",
+    default=".",
+  )
+  parser.add_argument(
+    "-c",
+    "--copy",
+    action="store_true",
+    help="copy instead of moving",
+  )
+  parser.add_argument(
+    "-d",
+    "--dry-run",
+    action="store_true",
+    help="do nothing",
+  )
+  parser.add_argument(
+    "-V",
+    "--verbose",
+    action="store_true",
+    help="log file actions",
+  )
+  parser.add_argument(
+    "-p",
+    "--prefix",
+    type=str,
+    help="the image prefix to use. Defaults to the source folder name",
+  )
+  args = parser.parse_args()
 
-    source_path = Path(args.source).resolve()
+  source_path = Path(args.source).resolve()
 
-    if not source_path.exists():
-        print(
-            f'Source folder "{source_path}" does not exist.',
-            file=sys.stderr,
-        )
-        exit(1)
+  if not source_path.exists():
+    print(f'Source folder "{source_path}" does not exist.', file=sys.stderr)
+    exit(1)
 
-    if not source_path.is_dir():
-        print(
-            f'Source "{source_path}" is not a folder.',
-            file=sys.stderr,
-        )
-        exit(1)
+  if not source_path.is_dir():
+    print(f'Source "{source_path}" is not a folder.', file=sys.stderr)
+    exit(1)
 
-    destination_path = Path(args.destination).resolve()
+  destination_path = Path(args.destination).resolve()
 
-    if not destination_path.exists():
-        print(
-            f'Destination folder "{destination_path}" does not exist.',
-            file=sys.stderr,
-        )
-        exit(1)
+  if not destination_path.exists():
+    print(f'Destination folder "{destination_path}" does not exist.', file=sys.stderr)
+    exit(1)
 
-    if not destination_path.is_dir():
-        print(
-            f'Destination "{destination_path}" is not a folder.',
-            file=sys.stderr,
-        )
-        exit(1)
+  if not destination_path.is_dir():
+    print(f'Destination "{destination_path}" is not a folder.', file=sys.stderr)
+    exit(1)
 
-    copy = args.copy
-    prefix = args.prefix or source_path.name
-    verbose = args.verbose
+  prefix = args.prefix or source_path.name
 
-    move_images(source_path, destination_path, copy, prefix, verbose)
+  move_images(source_path, destination_path, args.copy, prefix, args.verbose, args.dry_run)
